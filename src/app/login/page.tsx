ea0843511b1d0ce95bd2a20e0c9333555c42bbf2
Dikshant Wavhule <dikshantw11@gmail.com>
2025-10-23 12:18:50 +0000

    session and login page added

---
+++ src/app/login/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function LogInpage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  async function handleLogin(e: React.FormEvent) {
+    e.preventDefault();
+    setError("");
+
+    const res = await signIn("credentials", {
+      redirect: false,
+      email,
+      password,
+    });
+
+    if (res?.error) {
+      setError(res.error);
+    } else {
+      router.push("/dashboard");
+    }
+  }
+  return (
+    <div>
+      <h2>Log in</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button type="submit">Log In</button>
+        {error && <p className="text-red-500">{error}</p>}
+      </form>
+    </div>
+  );
+}
